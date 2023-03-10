4f78fb28e3b9b198a0adbe6f67afdc6622c00c2c
Estrella Abueva <estrella.abueva@cit.edu>
2023-03-10 23:50:20 +0800

    Add additional labels and textboxes

--- Team-G_BackPropagation/Team-G_BackPropagation/Form1.Designer.cs
+++ Team-G_BackPropagation/Team-G_BackPropagation/Form1.Designer.cs
@@ -32,11 +32,20 @@
             this.train = new System.Windows.Forms.Button();
             this.test = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bedroom = new System.Windows.Forms.TextBox();
+            this.bathroom = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.output = new System.Windows.Forms.Label();
+            this.living = new System.Windows.Forms.TextBox();
+            this.lot = new System.Windows.Forms.TextBox();
+            this.condition = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // create
@@ -73,29 +82,29 @@
             this.textBox1.Size = new System.Drawing.Size(163, 20);
             this.textBox1.TabIndex = 3;
             // 
-            // textBox2
+            // bedroom
             // 
-            this.textBox2.Location = new System.Drawing.Point(164, 207);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 20);
-            this.textBox2.TabIndex = 4;
+            this.bedroom.Location = new System.Drawing.Point(158, 207);
+            this.bedroom.Name = "bedroom";
+            this.bedroom.Size = new System.Drawing.Size(169, 20);
+            this.bedroom.TabIndex = 4;
             // 
-            // textBox3
+            // bathroom
             // 
-            this.textBox3.Location = new System.Drawing.Point(164, 234);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(169, 20);
-            this.textBox3.TabIndex = 5;
+            this.bathroom.Location = new System.Drawing.Point(158, 234);
+            this.bathroom.Name = "bathroom";
+            this.bathroom.Size = new System.Drawing.Size(169, 20);
+            this.bathroom.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 23);
+            this.label1.Location = new System.Drawing.Point(85, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.Size = new System.Drawing.Size(242, 20);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Housing Pricing";
+            this.label1.Text = "Housing Pricing in Melbourne";
             // 
             // label2
             // 
@@ -109,21 +118,105 @@
             // output
             // 
             this.output.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.output.Location = new System.Drawing.Point(50, 301);
+            this.output.Location = new System.Drawing.Point(47, 378);
             this.output.Name = "output";
             this.output.Size = new System.Drawing.Size(277, 61);
             this.output.TabIndex = 8;
+            // 
+            // living
+            // 
+            this.living.Location = new System.Drawing.Point(158, 261);
+            this.living.Name = "living";
+            this.living.Size = new System.Drawing.Size(169, 20);
+            this.living.TabIndex = 9;
+            // 
+            // lot
+            // 
+            this.lot.Location = new System.Drawing.Point(158, 288);
+            this.lot.Name = "lot";
+            this.lot.Size = new System.Drawing.Size(169, 20);
+            this.lot.TabIndex = 10;
+            // 
+            // condition
+            // 
+            this.condition.Location = new System.Drawing.Point(158, 315);
+            this.condition.Name = "condition";
+            this.condition.Size = new System.Drawing.Size(169, 20);
+            this.condition.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Number of Bedrooms";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Number of Bathrooms";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "SqFt_Living";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Lot";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 318);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Condition";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 356);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Possible House Price:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 399);
+            this.ClientSize = new System.Drawing.Size(375, 459);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.condition);
+            this.Controls.Add(this.lot);
+            this.Controls.Add(this.living);
             this.Controls.Add(this.output);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.bathroom);
+            this.Controls.Add(this.bedroom);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.test);
             this.Controls.Add(this.train);
@@ -142,11 +235,20 @@
         private System.Windows.Forms.Button train;
         private System.Windows.Forms.Button test;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox bedroom;
+        private System.Windows.Forms.TextBox bathroom;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label output;
+        private System.Windows.Forms.TextBox living;
+        private System.Windows.Forms.TextBox lot;
+        private System.Windows.Forms.TextBox condition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
