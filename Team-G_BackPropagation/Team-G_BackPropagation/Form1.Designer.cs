534640933379b6d2c6ca42b852a3527aad00f53c
Estrella Abueva <estrella.abueva@cit.edu>
2023-03-10 23:45:07 +0800

    Add buttons and all

--- Team-G_BackPropagation/Team-G_BackPropagation/Form1.Designer.cs
+++ Team-G_BackPropagation/Team-G_BackPropagation/Form1.Designer.cs
@@ -28,13 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.create = new System.Windows.Forms.Button();
+            this.train = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(26, 55);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(110, 40);
+            this.create.TabIndex = 0;
+            this.create.Text = "Create Model";
+            this.create.UseVisualStyleBackColor = true;
+            // 
+            // train
+            // 
+            this.train.Location = new System.Drawing.Point(26, 101);
+            this.train.Name = "train";
+            this.train.Size = new System.Drawing.Size(110, 40);
+            this.train.TabIndex = 1;
+            this.train.Text = "Train";
+            this.train.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(26, 147);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(110, 40);
+            this.test.TabIndex = 2;
+            this.test.Text = "Test";
+            this.test.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 207);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 234);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Housing Pricing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "How many epochs?";
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.output.Location = new System.Drawing.Point(50, 301);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(277, 61);
+            this.output.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(375, 399);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.train);
+            this.Controls.Add(this.create);
+            this.Name = "Form1";
+            this.Text = "Team-G_BackPropagation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Button train;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label output;
     }
 }
 
--- Team-G_BackPropagation/Team-G_BackPropagation/Form1.cs
+++ Team-G_BackPropagation/Team-G_BackPropagation/Form1.cs
@@ -16,5 +16,10 @@ namespace Team_G_BackPropagation
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
